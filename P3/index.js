980c78c5035412f47e8898b760c4fc59e9281842
alisson-go <79110107+alisson-go@users.noreply.github.com>
2021-05-10 11:53:36 +0200

    pruebas

--- P3/index.js
+++ P3/index.js
@@ -15,69 +15,90 @@ const LADRILLO = {
     padding: 5,
     visible: true
 };
+let x=0;
+let y=570;
+
+let velx=3;
+
+let velballx= 3;
+
+
 const ctx = canvas.getContext("2d");
 const ball=canvas.getContext("2d")
+function update()
+{
+  console.log("test");
+  if (x < 0 || x >= (canvas.width - 20) ) {
+    velx = -velx;
+  }
+  x = x + velx;
+  //-- 2) Borrar el canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  //-- 3) Dibujar los elementos visibles
+  ctx.beginPath();
+    ctx.rect(x, y, 40, 20);
+
+    //-- Dibujar
+    ctx.fillStyle = 'red';
+
+    //-- Rellenar
+    ctx.fill();
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+  ctx.closePath();
+
+  //-- 4) Volver a ejecutar update cuando toque
+  requestAnimationFrame(update);
 
 //-- Cada objeto a dibujar lo delimitaremos 
 //-- por los métodos beginPath() y closePath()
-ctx.beginPath();
-  //-- Definir un rectangulo de dimensiones 100x50,
-  //-- cuya esquina superior izquierda está en (5,5)
-  ctx.rect(400,560, 100, 40);
-  
 
-  //-- Color de relleno del rectángulo
-  ctx.fillStyle = 'orange';
+  ctx.beginPath();
+      ctx.arc(x,y,10,0,2*Math.PI);
+      ctx.strokeStyle = 'blue';
+      ctx.lineWidth = 3;
+      ctx.fillStyle = 'yellow';
 
-  //-- Mostrar el relleno
-  ctx.fill();
+      //-- Dibujar el trazo
+      ctx.stroke()
 
-  //-- Mostrar el trazo del rectángulo
-  ctx.stroke();
-ctx.closePath()
-ball.beginPath();
-    ball.arc(400,500,10,0,2*Math.PI);
-    ball.strokeStyle = 'blue';
-    ball.lineWidth = 3;
-    ball.fillStyle = 'yellow';
+      //-- Dibujar el relleno
+      ctx.fill()
 
-    //-- Dibujar el trazo
-    ball.stroke()
+  ctx.closePath()
+  const ladrillos = [];
 
-    //-- Dibujar el relleno
-    ball.fill()
-    
-ball.closePath()
-const ladrillos = [];
-
-for (let i = 0; i < LADRILLO.F; i++) {
-    ladrillos[i] = [];
-    for (let j = 0; j < LADRILLO.C; j++) {
-      ladrillos[i][j] = {
-          x: (LADRILLO.w + LADRILLO.padding) * j,
-          y: (LADRILLO.h + LADRILLO.padding) * i,
-          w: LADRILLO.w,
-          h: LADRILLO.h,
-          padding: LADRILLO.padding,
-          visible: LADRILLO.visible
-        };
-    }
-}
-
-//ladrillos[0][1].visible = false;
-
-
-//-- Dibujar ladrillos
-for (let i = 0; i < LADRILLO.F; i++) {
-    for (let j = 0; j < LADRILLO.C; j++) {
-
-      //-- Si el ladrillo es visible se pinta
-      if (ladrillos[i][j].visible) {
-        ctx.beginPath();
-        ctx.rect(ladrillos[i][j].x, ladrillos[i][j].y, LADRILLO.w, LADRILLO.h);
-        ctx.fillStyle = 'blue';
-        ctx.fill();
-        ctx.closePath();
+  for (let i = 0; i < LADRILLO.F; i++) {
+      ladrillos[i] = [];
+      for (let j = 0; j < LADRILLO.C; j++) {
+        ladrillos[i][j] = {
+            x: (LADRILLO.w + LADRILLO.padding) * j,
+            y: (LADRILLO.h + LADRILLO.padding) * i,
+            w: LADRILLO.w,
+            h: LADRILLO.h,
+            padding: LADRILLO.padding,
+            visible: LADRILLO.visible
+          };
       }
-    }
+  }
+
+  //ladrillos[0][1].visible = false;
+
+  //-- Dibujar ladrillos
+  for (let i = 0; i < LADRILLO.F; i++) {
+      for (let j = 0; j < LADRILLO.C; j++) {
+
+        //-- Si el ladrillo es visible se pinta
+        if (ladrillos[i][j].visible) {
+          ctx.beginPath();
+          ctx.rect(ladrillos[i][j].x, ladrillos[i][j].y, LADRILLO.w, LADRILLO.h);
+          ctx.fillStyle = 'blue';
+          ctx.fill();
+          ctx.closePath();
+        }
+      }
+  }
 }
+update()
