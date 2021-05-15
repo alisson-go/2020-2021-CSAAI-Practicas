2e38e6f8be1fe6018b6cbdf6b6b0e49034eb869b
alisson-go <79110107+alisson-go@users.noreply.github.com>
2021-05-15 17:49:25 +0200

    ladrillos

--- P3/index.js
+++ P3/index.js
@@ -6,13 +6,18 @@ const ctx = canvas.getContext("2d");
 //-- Definir el tamaño del convas
 canvas.width = 500;
 canvas.height = 600;
-
+//coordenadas para la pelota
 let x_ball=0;
 let y_ball=570;
+
 let velx=3;
 let velballx= 3;
+//coordenadas para la raqueta
 let x_racket= (canvas.width)/2;
 let y_racket=canvas.height-10;
+//coordenadas para los ladrillos
+let x_ladrillos = 20;
+let y_ladrillos = 20;
 
 const LADRILLO = {
     F: 4,  // Filas
@@ -30,8 +35,8 @@ for (let i = 0; i < LADRILLO.F; i++) {
     ladrillos[i] = [];
     for (let j = 0; j < LADRILLO.C; j++) {
       ladrillos[i][j] = {
-          x_ball: (LADRILLO.w + LADRILLO.padding) * j,
-          y_ball: (LADRILLO.h + LADRILLO.padding) * i,
+          x_ladrillos: (LADRILLO.w + LADRILLO.padding) * j,
+          y_ladrillos: (LADRILLO.h + LADRILLO.padding) * i,
           w: LADRILLO.w,
           h: LADRILLO.h,
           padding: LADRILLO.padding,
@@ -42,20 +47,7 @@ for (let i = 0; i < LADRILLO.F; i++) {
 
 //ladrillos[0][1].visible = false;
 
-//-- Dibujar ladrillos
-for (let i = 0; i < LADRILLO.F; i++) {
-    for (let j = 0; j < LADRILLO.C; j++) {
 
-      //-- Si el ladrillo es visible se pinta
-      if (ladrillos[i][j].visible) {
-        ctx.beginPath();
-        ctx.rect(ladrillos[i][j].x_ball, ladrillos[i][j].y_ball, LADRILLO.w, LADRILLO.h);
-        ctx.fillStyle = 'blue';
-        ctx.fill();
-        ctx.closePath();
-      }
-    }
-}
 function ball(){
   ctx.beginPath();
   ctx.arc(x_ball,y_ball,10,0,2*Math.PI);
@@ -85,7 +77,22 @@ function racket(){
   ctx.stroke()
   ctx.closePath();
 }
+function draw_ladrillos(){
+  //-- Dibujar ladrillos
+  for (let i = 0; i < LADRILLO.F; i++) {
+    for (let j = 0; j < LADRILLO.C; j++) {
 
+      //-- Si el ladrillo es visible se pinta
+      if (ladrillos[i][j].visible) {
+        ctx.beginPath();
+        ctx.rect(ladrillos[i][j].x_ladrillos, ladrillos[i][j].y_ladrillos, LADRILLO.w, LADRILLO.h);
+        ctx.fillStyle = 'blue';
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+}
 function update()
 {
   console.log("test");
@@ -97,17 +104,16 @@ function update()
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   //-- 3) Dibujar los elementos visibles
-  
+  //-- Dibujar ladrillos
+  draw_ladrillos();
   racket();
-  ball()
+  ball();
   //-- 4) Volver a ejecutar update cuando toque
 
   requestAnimationFrame(update);
 
 //-- Cada objeto a dibujar lo delimitaremos 
 //-- por los métodos beginPath() y closePath()
-
-
  
 }
 update()
