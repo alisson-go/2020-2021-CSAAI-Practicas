7988636ed375b72582964c66ab89c5753f169b23
alisson-go <79110107+alisson-go@users.noreply.github.com>
2021-05-26 00:27:20 +0200

    nuevos cambios

--- P2/index.js
+++ P2/index.js
@@ -1,29 +1,88 @@
-function insert(num)
+console.log("Ejecutando JS...");
+
+display = document.getElementById("display")
+suma = document.getElementById("suma")
+igual = document.getElementById("igual")
+clear = document.getElementById("clear")
+
+//-- Estados de la calculadora
+const ESTADO = {
+    INIT: 0,
+    OP1: 1,
+    OPERATION: 2,
+    OP2: 3
+}
+ 
+ //-- Variable de estado de la calculadora
+ //-- Al comenzar estamos en el estado incial
+ let estado = ESTADO.INIT;   
+
+//-- Función de retrollamada de los digitos
+function digito(ev)
 {
+    //-- Se ha recibido un dígito
+    //-- Según en qué estado se encuentre la calculadora
+    //-- se hará una cosa u otra
 
-    var digito = document.getElementById('resultado').innerHTML
-    document.getElementById('resultado').innerHTML = digito + num;
+    //-- Si es el primer dígito, no lo añadimos,
+    //-- sino que lo mostramos directamente en el display
+    if (estado == ESTADO.INIT) {
 
-}
-function clean(){
-    document.getElementById('resultado').innerHTML = " ";
-}
-function calcular(){
-    var resultado = document.getElementById('resultado').innerHTML;
-    var found = resultado.find(suma => suma.nombre==="++");
-    if(found == "++"){
-        console.log("holaaaa");
-    }else{
-        document.getElementById('resultado').innerHTML=eval(resultado)
-    }
+        display.innerHTML = ev.target.value;
 
-}
-function parcial(num){
-    var digito = document.getElementById('resultado').innerHTML
-    pos=digito.length;
-    digito=digito.substr(0,pos-1);
-    if(digito==" "){
-        digito="0";
+        //-- Pasar al siguiente estado
+        estado = ESTADO.OP1;
+
+    } else {
+       
+        //--En cualquier otro estado lo añadimos
+        display.innerHTML += ev.target.value;
+
     } 
-    document.getElementById('resultado').innerHTML=digito;
+    
+}
+
+
+//-- Obtener una colección con todos los elementos
+//-- de la clase digito
+digitos = document.getElementsByClassName("digito")
+
+//-- Establecer la misma función de retrollamada
+//-- para todos los botones de tipo dígito
+for (let boton of digitos) {
+
+    //-- Se ejecuta cuando se pulsa un boton
+    //-- que es un dígito. Para que el código sea 
+    //-- mas legible la función de retrollamada se
+    //-- escribe como una función normal (digito)
+    boton.onclick = digito;
+}
+
+//-------- Resto de funciones de retrollamada
+
+//-- Operación de sumar
+suma.onclick = (ev) => {
+
+    //-- Insertar simbolo de sumar
+    display.innerHTML += ev.target.value;
+  
+}
+
+//-- Evaluar la expresion
+igual.onclick = () => {
+  
+    //-- Calcular la expresión y añadirla al display
+    display.innerHTML = eval(display.innerHTML);
+
+    //-- ¡Ojo! Aquí se hace siempre!
+    //-- Sólo se debe permitar que eso se haga
+    //-- si se está en el estado final (OP2)
+  
+}
+
+//-- Poner a cero la expresion
+//-- Y volver al estado inicial
+clear.onclick = () => {
+  display.innerHTML = "0";
+  estado = ESTADO.INIT;
 }
