d34bb5accb223d4c0555cdc3a7c45d490b868221
alisson-go <79110107+alisson-go@users.noreply.github.com>
2021-06-01 23:13:04 +0200

    calc terminada

--- P2/index.js
+++ P2/index.js
@@ -2,6 +2,7 @@ console.log("Ejecutando JS...");
 
 display = document.getElementById("display")
 suma = document.getElementById("suma")
+resta = document.getElementById("resta")
 igual = document.getElementById("igual")
 multiplicar= document.getElementById("multiplicar")
 elevar= document.getElementById("elevar")
@@ -12,6 +13,8 @@ tecla_DEL = document.getElementById("borrar_ultimo")
 coma = document.getElementById("coma")
 
 digitos = document.getElementsByClassName("digito")
+const click = new Audio('bambu_1.mp3');
+const error = new Audio('error.mp3');
 //operandos = document.getElementsByClassName("operandos")
 
 //-- Estados de la calculadora
@@ -19,7 +22,8 @@ const ESTADO = {
     INIT: 0,
     OP1: 1,
     OPERATION: 2,
-    OP2: 3
+    OP2: 3,
+    coma: false,
 }
 //var operaciones=0;
 
@@ -37,12 +41,13 @@ function digito(ev)
 
         //-- Pasar al siguiente estado
         estado = ESTADO.OP1;
-        
+        click.play();
 
     } else {
        
         //--En cualquier otro estado lo añadimos
         display.innerHTML += ev.target.value;
+        click.play();
 
 
     } 
@@ -52,8 +57,10 @@ function digito(ev)
 for (let boton of digitos) {
 
     boton.onclick = digito;
+    click.play();
 }
 var operaciones =0;
+var comas = 0;
 function calc_elevar(){
     if (ESTADO==1||ESTADO==3){
         signo=eval(display.innerHTML.replace('^','**'))
@@ -68,11 +75,12 @@ multiplicar.onclick = (ev) => {
     estado= ESTADO.OPERATION;
     operaciones = operaciones + 1;
     if (operaciones>1){
+        error.play();
         display.innerHTML = "ERROR DE SINTAXIS";
         operaciones = 0;
         console.log(operaciones);
     }   
-  
+    click.play();
 }
 suma.onclick = (ev) => {
     //-- Insertar simbolo de sumar
@@ -81,17 +89,32 @@ suma.onclick = (ev) => {
     estado= ESTADO.OPERATION;
     operaciones = operaciones + 1;
     if (operaciones>1){
+        error.play();
         display.innerHTML = "ERROR DE SINTAXIS";
         operaciones = 0;
         console.log(operaciones);
     }   
-  
+    click.play();
+}
+resta.onclick = (ev) => {
+    click.play();
+    display.innerHTML += ev.target.value;
+    estado= ESTADO.OPERATION;
+    operaciones = operaciones + 1;
+    if (operaciones>1){
+        error.play();
+        display.innerHTML = "ERROR DE SINTAXIS";
+        operaciones = 0;
+        console.log(operaciones);
+    }   
+    
 }
 porcentaje.onclick=()=>{
-
+    click.play();
     display.innerHTML = (display.innerHTML/100);
     operaciones = operaciones + 1;
     if (operaciones>1){
+        error.play();
         display.innerHTML = "ERROR DE SINTAXIS";
         operaciones = 0;
         console.log(operaciones);
@@ -100,10 +123,11 @@ porcentaje.onclick=()=>{
 }
 raiz.onclick=()=>{
    
-
+    click.play();
     display.innerHTML = Math.sqrt(display.innerHTML);
     operaciones = operaciones + 1;
     if (operaciones>1){
+        error.play();
         display.innerHTML = "ERROR DE SINTAXIS";
         operaciones = 0;
         console.log(operaciones);
@@ -112,7 +136,7 @@ raiz.onclick=()=>{
 }
 //-- Evaluar la expresion
 igual.onclick = () => {
-  
+    click.play();
     //-- Calcular la expresión y añadirla al display
     display.innerHTML = eval(display.innerHTML);
     operaciones = 0
@@ -121,11 +145,13 @@ igual.onclick = () => {
 
 
 clear.onclick = () => {
+    click.play();
   display.innerHTML = "0";
   estado = ESTADO.INIT;
   operaciones=0;
 }
 tecla_DEL.onclick =()=>{
+    click.play();
     if (display.innerHTML=="0"){
         display.innerHTML = "0";
     }else{
@@ -135,4 +161,19 @@ tecla_DEL.onclick =()=>{
             estado = ESTADO.INIT;
         }
     }
+}
+coma.onclick = (ev) => {
+    //-- Insertar simbolo de sumar
+    click.play();
+    display.innerHTML += ev.target.value;
+    ESTADO.coma=true;
+    estado= ESTADO.coma;
+    comas = comas + 1;
+    if (comas>2){
+        error.play();
+        display.innerHTML = "ERROR DE SINTAXIS";
+        comas = 0;
+        console.log(comas);
+    }   
+  
 }
